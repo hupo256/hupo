b465ee03e928d1e14ca4992c94785af06bcd1bde
dongdong tong <dongdong.tong@dianrong.com>
2016-03-31 10:47:20 +0800

    add wpk

---
+++ webpack/main.js
@@ -0,0 +1,5 @@
+// require('./bootstrap.css');
+// require('./myapp.less');
+
+var img = document.createElement('img');
+img.src = require('./mca_shale.jpg');
---
+++ webpack/webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './build', // This is where images AND js will go
+    publicPath: 'https://www.dianrong.com/landing/wp-content/uploads/2016/01/', // This is used to generate URLs to e.g. images
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
+    ]
+  }
+};
