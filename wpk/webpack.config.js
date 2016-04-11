e716b4fa54100e4726cc8919401dd50f1c2ac830
dongdong tong <dongdong.tong@dianrong.com>
2016-04-11 11:59:59 +0800

    output in one dic

--- wpk/webpack.config.js
+++ wpk/webpack.config.js
@@ -13,14 +13,14 @@ module.exports = {
 	//输出配置
 	output : {
 		path : path.join(__dirname, 'build'),
-		filename : './js/[name].[chunkhash:8].js'
+		filename : '[name].[chunkhash:8].js'
 	},
 	
 	//所要用到的插件
 	plugins : [		
 		// new HtmlWebpackPlugin(),   //生成匹配的hash
-		new webpack.optimize.CommonsChunkPlugin('./js/common.js'),  //提取公共单元到 common.js 这个文件里
-		new ExtractTextPlugin("./css/[name].[chunkhash:8].css"),     //输出的css文件名
+		new webpack.optimize.CommonsChunkPlugin('common.js'),  //提取公共单元到 common.js 这个文件里
+		new ExtractTextPlugin("[name].[chunkhash:8].css"),     //输出的css文件名
 		new HtmlWebpackPlugin({                               //根据模板插入css/js等生成最终HTML
 			// favicon:'./src/img/favicon.ico',     //favicon路径
 			filename:'index.html',         //生成的html存放路径，相对于 path
@@ -41,8 +41,7 @@ module.exports = {
 			// {test: /\.css$/, loaders: ['style', 'css']},
 			{test: /\.scss$/, loader: "style!css!sass"},
 			{test: /\.less$/, loader: "style!css!less"},
-			// {test: /\.(png|jpg)$/, loader: 'url-loader?limit=1000&name=../img/[hash].[ext]'}  //小于8kb的直接转为base64
-			{test: /\.(png|jpg)$/, loader: 'url-loader?limit=1000&name=../img/[hash].[ext]'}  //小于8kb的直接转为base64
+			{test: /\.(png|jpg)$/, loader: 'url-loader?limit=1000&name=[hash].[ext]'}  //小于8kb的直接转为base64
 			// {test : /\.(png|jpg|gif)$/,loaders : [
 					// 'file?hash=sha512&digest=hex&name=[hash].[ext]',
 					// 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
